651b9e6d3daadad20f99221b907e4c8509b8d59c
OuwaCavern <emircan.acikgoz@hotmail.com>
2024-03-03 21:03:25 +0300

    added trims for user inputs

--- Form1.cs
+++ Form1.cs
@@ -63,6 +63,7 @@ namespace OrderCancellerApp
                 }
                 else
                 {
+                    siparisNOInput = siparisNOInput.Trim();
                     string updateQuery = $"declare @SiparisNoYaziniz nvarchar(100)='{siparisNOInput}'\r\nupdate POSSiparis set SiparisDurumu= 8,Odendi = 0 , Kapandi = 1, SysAktif=0 where SiparisNo=@SiparisNoYaziniz;\r\n\r\ninsert into SistemTarihce\r\n(KayitId\r\n,Tablo\r\n,Tarih\r\n,RowVersion\r\n,SysAktif\r\n,SysKayitTarihi\r\n,SysKaydedenKullanici\r\n,Aciklama)\r\n\r\nselect \r\ns.Id,'POSSiparis',GETDATE(),0,1,GETDATE(),'ManuelKapatildi','güncelleme' from POSSiparis s where s.SiparisNo=@SiparisNoYaziniz";
                     SqlCommand insertCommand = new SqlCommand(updateQuery, sqlConnection);
                     insertCommand.ExecuteNonQuery();
@@ -85,6 +86,7 @@ namespace OrderCancellerApp
                     string[] siparislerinTamami = siparisNOInput.Split(',');
                     foreach (string siparis in siparislerinTamami)
                     {
+                        siparis.Trim();
                         string updateQuery = $"declare @SiparisNoYaziniz nvarchar(100)='{siparis}'\r\nupdate POSSiparis set SiparisDurumu= 3,Odendi = 1 , Kapandi = 1, SysAktif=1 where SiparisNo=@SiparisNoYaziniz;\r\n\r\ninsert into SistemTarihce\r\n(KayitId\r\n,Tablo\r\n,Tarih\r\n,RowVersion\r\n,SysAktif\r\n,SysKayitTarihi\r\n,SysKaydedenKullanici\r\n,Aciklama)\r\n\r\nselect \r\ns.Id,'POSSiparis',GETDATE(),0,1,GETDATE(),'ManuelKapatildi','güncelleme' from POSSiparis s where s.SiparisNo=@SiparisNoYaziniz";
                         SqlCommand insertCommand = new SqlCommand(updateQuery, sqlConnection);
                         insertCommand.ExecuteNonQuery();
@@ -92,6 +94,7 @@ namespace OrderCancellerApp
                 }
                 else
                 {
+                    siparisNOInput = siparisNOInput.Trim();
                     string updateQuery = $"declare @SiparisNoYaziniz nvarchar(100)='{siparisNOInput}'\r\nupdate POSSiparis set SiparisDurumu= 3,Odendi = 1 , Kapandi = 1, SysAktif=1 where SiparisNo=@SiparisNoYaziniz;\r\n\r\ninsert into SistemTarihce\r\n(KayitId\r\n,Tablo\r\n,Tarih\r\n,RowVersion\r\n,SysAktif\r\n,SysKayitTarihi\r\n,SysKaydedenKullanici\r\n,Aciklama)\r\n\r\nselect \r\ns.Id,'POSSiparis',GETDATE(),0,1,GETDATE(),'ManuelKapatildi','güncelleme' from POSSiparis s where s.SiparisNo=@SiparisNoYaziniz";
                     SqlCommand insertCommand = new SqlCommand(updateQuery, sqlConnection);
                     insertCommand.ExecuteNonQuery();
